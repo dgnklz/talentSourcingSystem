de9f2062b074e515bb6693ff89401aee06161bf6
dgnklz <dogankilaz@gmail.com>
2023-04-03 07:46:59 +0300

    Global Validation Exception Added

--- src/main/java/com/dgnklz/talentsourcingsystem/TalentSourcingSystemApplication.java
+++ src/main/java/com/dgnklz/talentsourcingsystem/TalentSourcingSystemApplication.java
@@ -1,11 +1,23 @@
 package com.dgnklz.talentsourcingsystem;
 
+import com.dgnklz.talentsourcingsystem.core.results.ErrorDataResult;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ValidationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestControllerAdvice
 public class TalentSourcingSystemApplication {
 
     public static void main(String[] args) {
@@ -15,6 +27,22 @@ public class TalentSourcingSystemApplication {
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ErrorDataResult<>(validationErrors, "VALIDATION EXCEPTION");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(ValidationException exception) {
+        return new ErrorDataResult<>(exception.getMessage(), "VALIDATION EXCEPTION");
     }
 
 }
